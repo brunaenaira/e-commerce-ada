3d48bff6ef9ab0d996e4629be047c4296ecd1cc5
Bruna Ariane Felix da Silva <65984596+brunaenaira@users.noreply.github.com>
2024-03-19 20:32:13 +0000

    Adicionada a busca de itens

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import {BsSearch} from 'react-icons/bs';
+import { useContext, useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
 
 import './style.css';
+import { EcommerceContext } from '../../context/EcommerceContext';
+import MlApi from '../../service/MlApi';
 
-class SearchBar extends React.Component {
-    render() {
+export default function SearchBar() {
+    const { setProducts } = useContext(EcommerceContext);
+    const [searchValue, setSearchValue] = useState('');
 
-        return (
-            <form className="search-bar">
-                <input
-                    type="search"
-                    placeholder="Buscar produtos"
-                    className="search__input"
-                    required
-                />
+    const handleSearch = async (event) => {
+        event.preventDefault();
 
-                <button type="submit" className="search__button">
-                    <BsSearch/>
-                </button>
-            </form>
-        );
-    }
+        MlApi(searchValue).get("/sites/MLB/search")
+            .then(res => {
+                const products = res.data.results.map((product: {
+                    title: string;
+                    thumbnail: string;
+                    price: number;
+                }) => ({
+                    title: product.title, thumbnail: product.thumbnail, price: product.price
+                }));
+                setProducts(products);
+            })
+        setSearchValue('');
+    };
+
+    return (
+        <form className="search-bar" onSubmit={handleSearch}>
+            <input
+                type="search"
+                value={searchValue}
+                placeholder="Buscar produtos"
+                className="search__input"
+                onChange={({ target }) => setSearchValue(target.value)}
+                required
+            />
+
+            <button type="submit" className="search__button">
+                <BsSearch />
+            </button>
+        </form>
+    );
 }
-
-export default SearchBar;
