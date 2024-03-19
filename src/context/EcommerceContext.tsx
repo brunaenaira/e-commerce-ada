5b8c4c4df4ffce234756781a8f190be2db8d1f5b
Bruna Ariane Felix da Silva <65984596+brunaenaira@users.noreply.github.com>
2024-03-19 16:58:43 +0000

    Criado context to ecommerce

--- src/context/EcommerceContext.tsx
+++ src/context/EcommerceContext.tsx
@@ -1,7 +1,39 @@
-import React from "react";
-import {EcommerceContextProps} from "../@types/ecommerce";
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+interface Product {
+    tititle: string;
+    thumbnail: string;
+    price: number;
+}
+interface Cart{
+    products: Product[];
+}
+interface EcommerceContextType {
+    products: Product[];
+    setProducts: React.Dispatch<React.SetStateAction<Product[]>>;
+    cart: Cart;
+    setCartItems: React.Dispatch<React.SetStateAction<Cart>>;
+}
 
+const EcommerceContext = createContext<EcommerceContextType | undefined>(undefined);
 
-export const EcommerceContext = React.createContext<EcommerceContextProps | undefined>(
-    undefined
-);
+interface EcommerceProviderProps {
+    children: ReactNode;
+}
+
+function EcommerceProvider({ children }: EcommerceProviderProps) {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [cart, setCart] = useState<Cart>(Cart(products=[]));
+    return (
+        <EcommerceProvider.Provider value={{ vingador, setVingador , cart, setCart }}>
+            {children}
+        </EcommerceProvider.Provider>
+    );
+}
+
+function useEcommerce(): EcommerceContextType {
+    const context = useContext(EcommerceContext);
+    if (!context) {
+        throw new Error('useEcommerce deve ser usado dentro de um EcommerceProvider');
+    }
+    return context;
+}
